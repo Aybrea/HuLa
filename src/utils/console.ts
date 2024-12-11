9ae281798c606579b8aabb19e4e3eff39e6f7498
Aybrea <wrenchou22@gmail.com>
2024-12-11 15:02:37 +0800

    简化初始打印

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -2,9 +2,5 @@ import pkg from '../../package.json'
 
 /** 控制台打印版本信息 */
 export const consolePrint = () => {
-  console.log(
-    `%c 🍀 ${pkg.name} ${pkg.version}`,
-    'font-size:20px;border-left: 4px solid #13987f;background: #cef9ec;font-family: Comic Sans MS, cursive;color:#581845;padding:10px;border-radius:4px;',
-    `${pkg.author.url}`
-  )
+  console.log(`${pkg.author.url}`)
 }
